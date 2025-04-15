9768ea4f2c8db5ca8cc0d27047a1f7f13a06c95c
Cfathurrohman <cfathurrohman@gmail.com>
2025-04-15 09:40:29 +0700

    Update package.json: Add start and dev scripts, and update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,14 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
+require("./routes/auth.routes")(app);
 
-require('./routes/auth.routes')(app);
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to authentication application.' });
-});
-
-// Set port and start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
