2395bc1d7a5330bb395730a6c3c0b369f83464db
Haidarly2 <haidar.aly007@gmail.com>
2025-05-07 18:42:40 +0700

    Refactor authentication routes and move wallet-related routes to a new wallet.routes.js file

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,8 +2,6 @@ const { verifyToken } = require("../middleware/auth.middleware");
 const { checkSoftDelete } = require("../middleware/checkSoftDelete.middleware")
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/users.controller");
-const walletController = require("../controllers/wallet.controller");
-const financesController = require("../controllers/finances.controller");
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
@@ -29,18 +27,4 @@ module.exports = function (app) {
   app.get("/api/users", verifyToken, userController.getUsers);
   app.put("/api/users/:id", verifyToken, checkSoftDelete, userController.updateUser);
   app.delete("/api/users/:id", verifyToken, checkSoftDelete, userController.softDeleteUser);
-
-  // Wallet routes
-  app.post("/api/wallet", verifyToken, walletController.createWallet);
-  app.get("/api/wallet", verifyToken, walletController.getAllWallets);
-  app.get("/api/wallet/:id", verifyToken, walletController.getWalletById);
-  app.put("/api/wallet/:id", verifyToken, walletController.updateWallet);
-  app.delete("/api/wallet/:id", verifyToken, walletController.deleteWallet);
-
-  // Wallet Transaction routes
-  app.post("/api/wallet-transactions", verifyToken, financesController.createTransaction);
-  app.get("/api/wallet-transactions", verifyToken, financesController.getAllTransactions);
-  app.get("/api/wallet-transactions/:id", verifyToken, financesController.getTransactionById);
-  app.put("/api/wallet-transactions/:id", verifyToken, financesController.updateTransaction);
-  app.delete("/api/wallet-transactions/:id", verifyToken, financesController.deleteTransaction);
 };
