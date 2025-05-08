e2929e7dbb95e8893cf9d2114b641ddb9a6ec2c8
Haidarly2 <haidar.aly007@gmail.com>
2025-05-08 13:29:12 +0700

    Add endpoints to retrieve wallet balance and all wallets with balance

--- routes/wallet.routes.js
+++ routes/wallet.routes.js
@@ -24,4 +24,8 @@ module.exports = function (app) {
     app.get("/api/wallets/:walletId/transactions/:transactionId", verifyToken, walletTransactionController.getTransactionById);
     app.put("/api/wallets/:walletId/transactions/:transactionId", verifyToken, walletTransactionController.updateTransaction);
     app.delete("/api/wallets/:walletId/transactions/:transactionId", verifyToken, walletTransactionController.deleteTransaction);
+
+    app.get('/api/wallets/:walletId/balance', verifyToken, walletTransactionController.getWalletWithBalance);
+    app.get("/api/wallets-balances", verifyToken, walletTransactionController.getAllWalletsWithBalance);
+
 };
