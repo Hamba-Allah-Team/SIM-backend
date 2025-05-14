97807ca3dc0787862eff108ae58af4de8ccaf436
Cfathurrohman <cfathurrohman@gmail.com>
2025-05-14 21:16:32 +0700

    Feat: activation

---
+++ migrations/20250514111355-create-activations.js
@@ -0,0 +1,85 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('activations', {
+      activation_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'users',
+          key: 'user_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      mosque_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'mosques',
+          key: 'mosque_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      activation_type: {
+        type: Sequelize.ENUM('activation', 'extension'),
+        allowNull: false
+      },
+      username_input: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      transaction_number: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      proof_image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM('pending', 'approved', 'rejected'),
+        defaultValue: 'pending'
+      },
+      submitted_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      approved_at: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      admin_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'users',
+          key: 'user_id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('activations');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_activations_activation_type";');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_activations_status";');
+  }
+};
--- models/index.js
+++ models/index.js
@@ -28,7 +28,7 @@ db.wallet = require("./wallet.model.js")(sequelize, Sequelize);
 db.wallet_transaction = require("./wallet_transaction.model.js")(sequelize, Sequelize);
 db.contents = require("./content.model.js")(sequelize, Sequelize);
 db.activity = require("./activity.model.js")(sequelize, Sequelize);
-
+db.activations = require("./activation.model.js")(sequelize, Sequelize);
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
