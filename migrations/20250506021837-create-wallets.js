88b3190f91a192173c3fa7acc93721c0b206822a
Haidarly2 <haidar.aly007@gmail.com>
2025-05-23 09:51:33 +0700

    Add wallet_name field to wallets table in migration

--- migrations/20250506021837-create-wallets.js
+++ migrations/20250506021837-create-wallets.js
@@ -4,11 +4,36 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('wallets', {
-      wallet_id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-      mosque_id: { type: Sequelize.INTEGER, allowNull: false, references: { model: 'mosques', key: 'mosque_id' } },
-      wallet_type: { type: 'wallet_type_enum', allowNull: false },
-      created_at: { type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') },
-      updated_at: { type: Sequelize.DATE, defaultValue: Sequelize.literal('NOW()') }
+      wallet_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      mosque_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'mosques',
+          key: 'mosque_id'
+        },
+        onDelete: 'CASCADE'
+      },
+      wallet_name: {
+        type: Sequelize.STRING,
+        allowNull: false // agar user wajib beri nama
+      },
+      wallet_type: {
+        type: 'wallet_type_enum',
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      }
     });
   },
 
