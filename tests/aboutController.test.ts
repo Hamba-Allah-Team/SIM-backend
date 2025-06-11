1b2ce88d66542e757402c032e335a34c241d1f2d
DeaPutri17 <putrinastitidea@gmail.com>
2025-06-11 16:02:09 +0700

    feat : update mosque test

--- tests/aboutController.test.ts
+++ tests/aboutController.test.ts
@@ -1,5 +1,11 @@
 const db = require("../models");
-import { getAbout } from "../controllers/about.controller";
+import { getAbout, updateAbout } from "../controllers/about.controller";
+
+const fs = require("fs");
+const path = require("path");
+
+jest.mock("fs");
+jest.mock("path");
 
 jest.mock("../models", () => ({
   user: { findByPk: jest.fn() },
@@ -66,6 +72,118 @@ describe("getAbout", () => {
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.send).toHaveBeenCalledWith({
       message: "Gagal mengambil data masjid.",
+    });
+  });
+});
+
+// update mosque
+describe("updateAbout", () => {
+  let req: any;
+  let res: any;
+
+  beforeEach(() => {
+    req = {
+      userId: 1,
+      body: {
+        name: "Masjid Contoh",
+        address: "Jl. Raya",
+      },
+      file: null,
+    };
+
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+
+    // Mock user & mosque
+    db.user.findByPk.mockResolvedValue({ mosque_id: 123 });
+    db.mosques.findByPk.mockResolvedValue({
+      image: "lama.jpg",
+      update: jest.fn().mockResolvedValue(true),
+    });
+
+    // Mock fs
+    fs.existsSync.mockReturnValue(true);
+    fs.unlinkSync.mockImplementation(() => {});
+  });
+
+  it("should update successfully", async () => {
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Profil masjid berhasil diperbarui.",
+    });
+  });
+
+  it("should return 400 if email invalid", async () => {
+    req.body.email = "salah@";
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Format email tidak valid.",
+    });
+  });
+
+  it("should return 400 if phone number invalid", async () => {
+    req.body.phone_whatsapp = "123abc";
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Nomor WhatsApp harus berupa angka dan minimal 10 digit.",
+    });
+  });
+
+  it("should return 400 if longitude invalid", async () => {
+    req.body.longitude = "abc";
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Longitude harus berupa string angka desimal yang valid.",
+    });
+  });
+
+  it("should return 400 if latitude invalid", async () => {
+    req.body.latitude = "koordinat";
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Latitude harus berupa string angka desimal yang valid.",
+    });
+  });
+
+  it("should return 404 if user not found", async () => {
+    db.user.findByPk.mockResolvedValue(null);
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Pengguna tidak ditemukan.",
+    });
+  });
+
+  it("should return 404 if mosque not found", async () => {
+    db.mosques.findByPk.mockResolvedValue(null);
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Data masjid tidak ditemukan.",
+    });
+  });
+
+  it("should return 500 if update throws error", async () => {
+    db.mosques.findByPk.mockRejectedValue(new Error("DB error"));
+    await updateAbout(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Terjadi kesalahan saat memperbarui profil masjid.",
     });
   });
 });
